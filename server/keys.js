73f5347bd88b5b6dcbc38130875fafe85458832b
Boris Tsao <boristsao@Boriss-MacBook-Pro.local>
2022-04-26 17:13:48 -0700

    second commit

--- server/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  pgUser: process.env.PGUSER,
-  pgHost: process.env.PGHOST,
-  pgDatabase: process.env.PGDATABASE,
-  pgPassword: process.env.PGPASSWORD,
-  pgPORT: process.env.PGPORT,
-};
