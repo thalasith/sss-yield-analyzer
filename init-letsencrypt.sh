3d35cd7c9c49596fcc00d91bed272ec8f76a1529
Boris Tsao <boristsao@Boriss-MacBook-Pro.local>
2022-05-05 17:50:02 -0700

    trying to do ssl again

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -3,7 +3,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(skellieyieldanalyzer.com www.skellieyieldanalyzer.com)
+domains="skellieyieldanalyzer.com"
 rsa_key_size=4096
 data_path="./data/certbot"
 email="test@test.com" # Adding a valid address is strongly recommended
